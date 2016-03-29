b7292cf9f926c96baab2679c758a54f149bcf161
sartxi <sean.archibeque@gmail.com>
2016-03-29 16:44:38 -0600

    avatars

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -10,7 +10,7 @@
      */
 
 
-    function SettingsCtrl($state, request, $sessionStorage, $window, growl) {
+    function SettingsCtrl($state, request, $sessionStorage, $window, growl, $uibModal) {
         var self = this;
 
         function init() {
@@ -71,6 +71,32 @@
             });
         }
 
+        self.changeAvatar = function () {
+            var avatarModal = $uibModal.open({
+                animation: true,
+                size: 'lg',
+                templateUrl: 'views/partials/avatars.html',
+                controller: 'AvatarGallCtrl',
+                windowClass: 'normalModal'
+            });
+
+            avatarModal.result.then(function (newAvatar) {
+                var data = {
+                    id: self.userSettings.id,
+                    lsMenu: self.userSettings.lsMenu,
+                    avatar: newAvatar.avatar,
+                    user: self.user.id
+                }
+                request.name = 'userSettings';
+                request.update(data.id, data).then(function () {
+                    growl.add('success', 'Your settings are Saved!', 3000);
+                    $window.location.reload();
+                }, function () {
+                    growl.add('danger', 'Could not save settings!', 3000);
+                });
+            });
+        }
+
         self.logout = function () {
             request.name = 'users';
             request.logout();
@@ -80,6 +106,42 @@
         init();
     }
 
+    function AvatarGallCtrl($scope, $uibModalInstance) {
+        function init() {
+            $scope.title = 'User Avatars';
+        }
+
+        $scope.avatars = [
+            {avatar: '/assets/images/avatars/avatar1.jpg'},
+            {avatar: '/assets/images/avatars/avatar2.jpg'},
+            {avatar: '/assets/images/avatars/avatar3.jpg'},
+            {avatar: '/assets/images/avatars/avatar4.jpg'},
+            {avatar: '/assets/images/avatars/avatar5.jpg'},
+            {avatar: '/assets/images/avatars/avatar6.jpg'},
+            {avatar: '/assets/images/avatars/avatar7.jpg'},
+            {avatar: '/assets/images/avatars/avatar8.jpg'},
+            {avatar: '/assets/images/avatars/avatar9.jpg'},
+            {avatar: '/assets/images/avatars/avatar10.jpg'},
+            {avatar: '/assets/images/avatars/avatar11.jpg'},
+            {avatar: '/assets/images/avatars/avatar12.jpg'},
+            {avatar: '/assets/images/avatars/avatar13.jpg'},
+            {avatar: '/assets/images/avatars/avatar14.jpg'},
+            {avatar: '/assets/images/avatars/avatar15.jpg'},
+            {avatar: '/assets/images/avatars/avatar16.jpg'}
+        ]
+
+        $scope.useAvatar = function (avatar) {
+            $uibModalInstance.close(avatar);
+        }
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+
+        init();
+    }
+
     angular.module('ezadmin')
-        .controller('SettingsCtrl', ['$state', 'request', '$sessionStorage', '$window', 'growl', SettingsCtrl]);
+        .controller('SettingsCtrl', ['$state', 'request', '$sessionStorage', '$window', 'growl', '$uibModal', SettingsCtrl])
+        .controller('AvatarGallCtrl', ['$scope', '$uibModalInstance', AvatarGallCtrl]);
 })();
